737c75f49fd2948e48dabd6418017e327087832a
Hycarus <mirkomarasco08@gmail.com>
2023-10-23 15:18:02 +0200

    created scaffholding

---
+++ js/script.js
@@ -0,0 +1,10 @@
+(function (){
+    const btn = document.getElementById('invia');
+    const btnReset = document.getElementById('reset');
+    const wrapper = document.getElementById('wrapper');
+    const input = document.getElementById('input');
+    const result = document.querySelector('.result')
+    
+}) ();
+
+
