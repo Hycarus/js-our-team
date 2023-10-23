292536d7dbdc3c9fab237c68c45dde52d24d7160
Hycarus <mirkomarasco08@gmail.com>
2023-10-23 16:00:03 +0200

    added input for new member

--- js/script.js
+++ js/script.js
@@ -1,41 +1,38 @@
 "use strict";
 (function (){
     const btn = document.getElementById('invia');
-    const btnReset = document.getElementById('reset');
-    const wrapper = document.getElementById('wrapper');
-    const input = document.getElementById('input');
     const rowEl = document.querySelector('.row')
     
     const teamMember = [
         {
             name: 'Wayne Barnett',
             role: 'Founder & CEO',
-            picture: '../img/wayne-barnett-founder-ceo.jpg',
+            picture: 'wayne-barnett-founder-ceo.jpg',
         },
         {
             name: 'Angela Caroll',
             role: 'Chief Editor',
-            picture: '../img/angela-caroll-chief-editor.jpg',
+            picture: 'angela-caroll-chief-editor.jpg',
         },
         {
             name: 'Walter Gordon',
             role: 'Office Manager',
-            picture: '../img/walter-gordon-office-manager.jpg',
+            picture: 'walter-gordon-office-manager.jpg',
         },
         {
             name: 'Angela Lopez',
             role: 'Social Media Manager',
-            picture: '../img/angela-lopez-social-media-manager.jpg',
+            picture: 'angela-lopez-social-media-manager.jpg',
         },
         {
             name: 'Scott Estrada',
             role: 'Developer',
-            picture: '../img/scott-estrada-developer.jpg',
+            picture: 'scott-estrada-developer.jpg',
         },
         {
             name: 'Barbara Ramos',
             role: 'Graphic Designer',
-            picture: '../img/barbara-ramos-graphic-designer.jpg',
+            picture: 'barbara-ramos-graphic-designer.jpg',
         },
     ];
     for(const val in teamMember){
@@ -45,8 +42,8 @@
         const col = document.createElement("div");
         col.classList.add("col-4");
         const template = `
-              <div class="card text-start">
-                <img class="card-img-top" src="${teamMember.picture}" alt="Title">
+              <div class="card text-center m-3">
+                <img class="card-img-top" src="img/${teamMember.picture}" alt="Title">
                 <div class="card-body">
                   <h4 class="card-title">${teamMember.name}</h4>
                   <p class="card-text">
@@ -58,7 +55,24 @@
           col.innerHTML = template;
       
           rowEl.append(col);
-      }
+    }
+
+    btn.addEventListener('click', function(){
+        const newMember = {
+            name: document.getElementById('name').value,
+            role: document.getElementById('role').value,
+            picture: document.getElementById('picture').value,
+        }
+        teamMember.push(newMember);
+        printCol(newMember);
+        reset();
+    })
+
+    function reset(){
+        document.getElementById('name').value = '';
+        document.getElementById('role').value = '';
+        document.getElementById('picture').value= '';
+    }
 }) ();
 
 
