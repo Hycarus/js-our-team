732bfd7cda3d1528adf724cee4a52268dd29796f
Hycarus <mirkomarasco08@gmail.com>
2023-10-23 15:25:09 +0200

    created array with object team member

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+"use strict";
 (function (){
     const btn = document.getElementById('invia');
     const btnReset = document.getElementById('reset');
@@ -5,6 +6,38 @@
     const input = document.getElementById('input');
     const result = document.querySelector('.result')
     
+    const teamMember = [
+        {
+            name: 'Wayne Barnett',
+            role: 'Founder & CEO',
+            picture: 'wayne-barnett-founder-ceo.jpg',
+        },
+        {
+            name: 'Angela Caroll',
+            role: 'Chief Editor',
+            picture: 'angela-caroll-chief-editor.jpg',
+        },
+        {
+            name: 'Walter Gordon',
+            role: 'Office Manager',
+            picture: 'walter-gordon-office-manager.jpg',
+        },
+        {
+            name: 'Angela Lopez',
+            role: 'Social Media Manager',
+            picture: 'angela-lopez-social-media-manager.jpg',
+        },
+        {
+            name: 'Scott Estrada',
+            role: 'Developer',
+            picture: 'scott-estrada-developer.jpg',
+        },
+        {
+            name: 'Barbara Ramos',
+            role: 'Graphic Designer',
+            picture: 'barbara-ramos-graphic-designer.jpg',
+        },
+    ]
 }) ();
 
 
